275b6d81e4477411adb0d1e8506aa3a184adc85d
AnawaratneMANA <it19086408@my.sliit.lk>
2021-11-02 16:38:18 +0530

    View Refactored

--- app/src/main/java/com/example/firebase_recycler_view/RecycleViewClass.java
+++ app/src/main/java/com/example/firebase_recycler_view/RecycleViewClass.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RecycleView extends AppCompatActivity {
+public class RecycleViewClass extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
